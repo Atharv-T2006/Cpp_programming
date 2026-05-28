d3e63c92362218d6d744274dac5055abdac0c546
Atharv-T2006 <atharvtimane1@gmail.com>
2026-05-28 22:37:21 +0530

    Add files via upload
    14 /04

---
+++ Downcasting.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Base
+{
+   public:
+        int i,j;  
+};    //8 byte
+class Derived : public Base
+{
+    public: 
+        int x,y;
+
+};       //16 byte
+int main()
+{
+    Derived * dp = NULL;
+    Base bobj;
+    dp = & bobj;   // downcasting // which gives error
+
+
+    return 0;
+}
